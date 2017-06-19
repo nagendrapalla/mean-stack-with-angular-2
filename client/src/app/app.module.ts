062b490330939281ecb5e10358e1c1d4be4444b7
Nagendra Palla <support@stallats.in>
2017-06-20 04:55:52 +0530

    Routing Setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, DashboardComponent],
+  imports: [BrowserModule, FormsModule, HttpModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
